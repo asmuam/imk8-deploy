662678e1522614b3ef55d453547c189a0768a26d
ADIB SULTHON MUAMMAL <222111840@stis.ac.id>
2024-06-21 23:27:52 +0700

    search n berita dinamis data ada di JS

--- assets/js/berita.js
+++ assets/js/berita.js
@@ -1,4 +1,3 @@
-
 const path = window.location.pathname;
 const segments = path.split('/');
 let judul = segments[segments.length - 1]; // Ambil bagian terakhir dari path
@@ -24,5 +23,113 @@ judul = decodeURIComponent(judul);
 
 const judulBreadcrumb = document.getElementById("judul-berita-bc");
 const judulBerita = document.getElementById("judul-berita");
-judulBreadcrumb.textContent = judul;
-judulBerita.textContent = judul;
+
+if (judulBerita!==null){
+    judulBerita.textContent = judul;
+} 
+if (judulBreadcrumb!==null) {
+    judulBreadcrumb.textContent = judul;
+}
+
+
+function carouselBerita() {
+    return {
+        slides: beritaData.slice(0,4),
+        activeSlide: 0,
+        interval: null,
+
+        next() {
+            this.activeSlide = (this.activeSlide + 1) % this.slides.length;
+            this.scrollToSlide(this.activeSlide);
+        },
+        prev() {
+            this.activeSlide = (this.activeSlide - 1 + this.slides.length) % this.slides.length;
+            this.scrollToSlide(this.activeSlide);
+        },
+        goToSlide(index) {
+            this.activeSlide = index;
+            this.scrollToSlide(this.activeSlide);
+        },
+        scrollToSlide(index) {
+            this.$refs.carousel.scrollTo({
+                left: this.$refs.carousel.offsetWidth * index,
+                behavior: 'smooth'
+            });
+        },
+        autoSlide() {
+            this.stopSlide(); // Ensure no multiple intervals are running
+            this.interval = setInterval(() => {
+                this.next();
+            }, 10000); // Change slide every 10 seconds
+        },
+        stopSlide() {
+            clearInterval(this.interval);
+        },
+        init() {
+            this.autoSlide();
+        }
+    };
+}
+
+
+
+function popularArticle() {
+        let tinggiKonten = document.getElementById("konten").offsetHeight;
+        let sideContentNum = Math.ceil(tinggiKonten / 600);
+        let numArticle, numVideo, numPhoto;
+        if (sideContentNum < 3) {
+        numArticle = sideContentNum * 3;
+            if (numArticle < 6) {
+                numArticle = 5;
+            }
+        } else {
+        numArticle = sideContentNum;
+        }
+        if (numArticle > 3) {
+        document.getElementById("popular-image").classList.add("hidden");
+        document.getElementById("popular-video").classList.add("hidden");
+        }
+        popular = beritaData.slice(5, numArticle+4)
+            return {
+              slides: popular
+            }
+          }
+
+function popularVideo() {
+        let tinggiKonten = document.getElementById("konten").offsetHeight;
+        let sideContentNum = Math.ceil(tinggiKonten / 600);
+        let numVideo;
+
+        if (sideContentNum < 3) {
+            
+        } else {
+        numVideo = sideContentNum;
+        }
+        if (sideContentNum < 3) {
+        document.getElementById("popular-video").classList.add("hidden");
+        }
+          popular = galleryVideo.slice(0, numVideo-1)
+            return {
+              slides: popular
+            }
+          }
+
+function popularImage() {
+            let tinggiKonten = document.getElementById("konten").offsetHeight;
+        let sideContentNum = Math.ceil(tinggiKonten / 600);
+        let numVideo;
+
+        if (sideContentNum < 3) {
+            
+        } else {
+        numVideo = sideContentNum;
+        }
+
+        if (sideContentNum < 3) {
+        document.getElementById("popular-image").classList.add("hidden");
+        }
+          popular = galleryData.slice(0, numVideo-1)
+            return {
+              slides: popular
+            }
+          }
--- assets/js/beritaData.js
+++ assets/js/beritaData.js
@@ -4,7 +4,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: " 20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -21,7 +23,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -38,7 +42,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -55,7 +61,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -72,7 +80,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -89,7 +99,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -106,7 +118,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -123,7 +137,9 @@ const beritaData = [
         url: "/Pj. Wali Kota Bekasi Resmi Luncurkan Aplikasi E-Open 2.0.html",
         img: "https://bekasikota.go.id/uploads/post/841fb0eead10e6feb1f5cb1fccdf2ab6.jpg",
         overview: "Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya. ",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "20 Juni 2024 ",
+        author: "By Admin",
+        views: "25 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
@@ -140,7 +156,28 @@ const beritaData = [
         url: "/Asda II Kota Bekasi Minta LAZ Attaqwa Membangun Kesejahteraan Masyarakat Bekasi Raya",
         img: "https://bekasikota.go.id/uploads/post/3b9c6119823e4dd1168fcb75d73d9fd1.jpg",
         overview: "KOTA BEKASI - Yayasan Attaqwa Pusat menggelar Ijtima' Sanawi sekaligus launching Lembaga Amil Zakat (LAZ) Attaqwa, bertempat di Auditorium Pondok Pesantren Putra, Bekasi Kamis (20/6/2024). Asisten Daerah Asda II Pemerintah Kota Bekasi bidang pembangunan dan kesejahteraan, Inayatullah, menyebut amanah yang diberikan kepada LAZ Attaqwa adalah tugas yang mulia. Ia berharap LAZ Attaqwa bisa semakin sukses ke depannya.",
-        stamp: " By Admin  20 Juni 2024  25 Views",
+        time: "21 Juni 2024 ",
+        author: "By Admin",
+        views: "9 Views",
+        tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
+        isi: `
+        Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
+        Aplikasi E-Open 2.0 resmi diluncurkan oleh Pj. Wali Kota Bekasi, Gani Muhamad pada Kamis (20/06) dan masyarakat sudah bisa mengaksesnya.\n\n
+        Mengedepankan _website based application_, kehadiran E-Open 2.0 semakin memudahkan masyarakat. Tanpa perlu mengunduh aplikasi, cukup mengakses laman www.e-open.id, masyarakat sudah bisa menikmati berbagai layanan yang tersedia.\n\n
+        Menurut Kepala Disdukcapil, Taufiq Hidayat, "dengan berbasis _website_, masyarakat tidak perlu lagi mengunduh aplikasi yang tidak semua _platform_ atau jenis _smartphone_ bisa mengunduhnya, namun dengan mengakses website, siapapun dan berbagai _device_ bisa dengan mudah serta cepat mendapatkan berbagai layanan adminduk yang tersedia," ucapnya.\n\n
+        Pj. Wali Kota Bekasi, Gani Muhamad tentunya mendukung penuh hadirnya E-Open 2.0. Gani Muhamad menekankan bahwa, "semakin ke sini, semakin majunya perkembangan teknologi, kebutuhan layanan untuk masyarakat musti harus terus dikembangkan, apalagi terkait adminduk yang merupakan kebutuhan utama sebagai Warga Negara Indonesia, harus tersedia bentuk layanan yang aksesnya mudah, tidak merepotkan, cepat, dan tidak sulit tahapannya, sehingga masyarakat pun merasa nyaman akan pelayanan yang kita berikan," tegas Gani Muhamad.\n\n
+        Inovasi pelayanan adminduk E-Open sebelumya telah berhasil meraih berbagai penghargaan sampai dengan tingkat Nasional. Disdukcapil juga menghadirkan program-program layanan adminduk lainnya, namun Gani Muhamad tetap berpesan untuk tidak berhenti ciptakan inovasi-inovasi pelayanan lainnya.\n\n
+        "Saya sangat mengapresiasi atas segala raihan prestasi yang telah ditorehkan atas inovasi-inovasi program layanan yang dihadirkan Disdukcapi, dan saya pun mendorong Kadisdukcapil dan segenap jajarannya untuk tidak berhenti berinovasi menciptakan program-program terbaik lainnya untuk kemajuan pelayanan publik di Kota Bekasi ke depannya," tutup Gani (Wan/Humas)
+        `,
+    },
+    {
+        title: "Asda II Kota Bekasi Minta LAZ Attaqwa Membangun Kesejahteraan Masyarakat Bekasi Raya",
+        url: "/Asda II Kota Bekasi Minta LAZ Attaqwa Membangun Kesejahteraan Masyarakat Bekasi Raya",
+        img: "https://bekasikota.go.id/uploads/post/3b9c6119823e4dd1168fcb75d73d9fd1.jpg",
+        overview: "KOTA BEKASI - Yayasan Attaqwa Pusat menggelar Ijtima' Sanawi sekaligus launching Lembaga Amil Zakat (LAZ) Attaqwa, bertempat di Auditorium Pondok Pesantren Putra, Bekasi Kamis (20/6/2024). Asisten Daerah Asda II Pemerintah Kota Bekasi bidang pembangunan dan kesejahteraan, Inayatullah, menyebut amanah yang diberikan kepada LAZ Attaqwa adalah tugas yang mulia. Ia berharap LAZ Attaqwa bisa semakin sukses ke depannya.",
+        time: "21 Juni 2024 ",
+        author: "By Admin",
+        views: "9 Views",
         tags: "E-Open 2.0, Disdukcapil, PJ. Wali Kota Bekasi",
         isi: `
         Kota Bekasi - Guna memaksimalkan dan memperbaharui pelayanan Administrasi Kependudukan bagi masyarakat secara _online_ yang bisa diakses darimana saja, kini Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) menerbitkan Aplikasi E-Open 2.0 yang merupakan _upgrade_ dari sistim sebelumnya.\n\n
