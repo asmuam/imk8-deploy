c2d3d132a3a83b54460189bdc8fe864a02417c24
ADIB SULTHON MUAMMAL <222111840@stis.ac.id>
2024-06-22 18:47:16 +0700

    adib fix ke sekian kali

---
+++ assets/js/bangunan.js
@@ -0,0 +1,101 @@
+const bangunanData = [
+    {
+        title: "Monumen Kali Bekasi",
+        isi: 
+        `Insiden Kali Bekasi memiliki arti yang sangat dalam bagi rakyat Bekasi, menggambarkan keberanian rakyat Bekasi, sekaligus tragis. Kali Bekasi merupakan garis demarkasi antara tentara sekutu (Inggris dan NICA) yang menduduki Jakarta dengan laskar-laskar republik yang bertahan di seberang kali dibagian timur.
+
+        Akibat pendudukan Jepang yang kejam terhadap rakyat Bekasi. Pemuda dan rakyat Bekasi bertindak sendiri dengan menangkap orang-orang Jepang atau bahkan siapa saja yang diduga telah bekerjasama dengan Jepang. Pemuda dan rakyat Bekasi menghentikan setiap kereta api yang melintas ke Bekasi baik yang keluar maupun ke Jakarta.
+
+        Pada 19 Oktober 1945 meluncur kereta dari Jakarta yang mengangkut tawanan Jepang menuju Ciater, kereta itu pun lolos dari hadangan, setibanya di Cikampek dihentikan oleh para pejuang setempat, dan diperintahkan untuk kembali ke Jakarta. Ternyata rakyat Bekasi sudah menunggu. Sesampainya di Stasiun Bekasi seluruh gerbong digeledah dan ditemukan 90 orang tentara Jepang.
+
+        Rakyat beringas ketika ditemukan senjata api milik seorang tawanan, kemudian seluruh tentara Jepang wajib menyerahkan persenjataannya. Disitu juga, seluruh tawanan ditempatkan di Rumah Gadai tepi Kali Bekasi yang dijadikan lokasi penjara sementara. Padahal, awak kereta ketika itu sudah mencegah penggeledahan terhadap tawanan dengan menunjukkan surat perintah jalan dari Menteri Subardjo yang ditandatangani Bung Karno.
+
+        Rakyat Bekasi sepertinya tak mempedulikan. Kemarahannya sudah memuncak karena pengalaman sejarah yang begitu kejam pada masa kependudukan Jepang. Seusai Maghrib, seluruh tawanan digelandang ke tepi Kali Bekasi kemudian Kali Bekasi yang jernih kemudian ditutupi darah.
+
+        Kini, Monumen Kali Bekasi dibangun dan ditempatkan di pinggir Kota Bekasi, dekat jembatan menghubungkan Kecamatan Bekasi Selatan dengan Bekasi Timur. Monumen itu dibuat untuk memperingati insiden Kali Bekasi. Pembangunannya atas dasar kerjasama Pemerintah Kota Bekasi dengan pemerintah Jepang yang menggambarkan pesan perdamaian dan cinta kasih.`,
+        img: "https://bekasikota.go.id/uploads/gallery/bf4ba7b676ce675957960765e6fb6676.jpg"
+    },
+    {
+        title: "Rumah Panggung",
+        isi: `Pada mulanya rumah masyarakat Bekasi adalah rumah panggung yang bercirikan arsitektur Melayu., sebab pada bagian atapnya terdapat lembayung. Ciri ini masih tampak pada rumah di Desa Cikedokan, Bekasi yang diduga didirikan oleh pangeran Sake pada akhir abad ke 17.
+
+        Rumah-rumah ini berdiri di tepi sungai, karena pada mulanya sumber kehidupan terdapat di sungai. Pola rumah panggung pada orang Melayu Betawi dengan demikian mempunyai fungsi mengantisipasi banjir, mengandung makna sakral, dan sarana diskursus intelektual. Bentuk rumah panggung bagi masyarakat yang berdiam di Daerah Aliran Sungai (DAS) sangatlah berguna untuk menyelamatkan kehidupan ketika datang musim hujan.`,
+        img: "https://bekasikota.go.id/uploads/gallery/23517befb80e149d348493abb57e60a1.jpg"
+    },
+    {
+        title: "Rumah Kebaya",
+        isi: `Rumah yang menjajak ke bumi, selanjutnya lebih disukai karena proses pembuatannya yang lebih sederhana. Namun, lantai dibuat lebih tinggi dari permukaan tanah, sehingga bala suji sebagai unsur pendukung tetap dipertahankan. Pada daerah gunung seperti Gunung Puteri yang masyarakatnya menggunakan bilingual Melayu Betawi dan Sunda. Pola rumah panggung digunakan untuk bangunan suci.
+
+        Penggunaan pola panggung di sini lebih bersifat sakral daripada fungsional. Bangunan suci, baik pada tradisi pra Islam maupun Islam, cenderung berbentuk panggung mengikuti pola Bale Kembang tempat peristirahatan raja dan keluarganya. Rumah masyarakat di daerah Bekasi hingga tahun 1950 masih menggunakan bahan pokok bambu.
+
+        Pagar halaman umumnya dari bambu yang disebut ‘Pagar Jaro’ yang disilang secara khas. Biasanya, ‘Jaro’ diselingi pohon sebagai penyangga. Yang paling sering jenis pohon yang dijadikan penyangga adalah Pohon Jarak, sujen, atau Pohon Jambu Batu.`,
+        img: "https://bekasikota.go.id/uploads/gallery/7cf69b350ce170bad369b3b996e16f03.jpg"
+    },
+    {
+        title: "Monumen Alun-Alun",
+        isi: `Monumen ini didirikan pada tanggal 5 Juli 1955 dan terletak di Jalan Veteran Kota Bekasi atau lebih tepatnya di depan Markas Kodim 0507 Bekasi. Bentuk dari monumen ini adalah persegi lima dan terbuat dari batu bata dengan tinggi 5.08 centimeter, termasuk dasar tugu yang dikelilingi oleh pagar tembok dengan tinggi 1 meter. Dan masing-masing 3 meter (berbentuk segi lima dengan pengertian Pancasila).
+
+        Monumen ini didirikan untuk memperingati berbagai peristiwa yang terjadi di Bekasi, di antaranya:
+
+        a. Peristiwa bulan Agustus 1945 (bertepatan dengan Proklamasi Kemerdekaan RI).<br>
+        b. Peristiwa awal bulan Februari 1950 (penentuan Revolusi Rakyat Bekasi-keinginan Rakyat Bekasi).
+
+        Tugu ini didirikan pada masa peringatan Bupati Abdul Fatah 1975. Dan diresmikannya oleh Gubernur Daerah tingkat I Provinsi Jawa Barat dan terletak di Jalan Jenderal Ahmad Yani. Bentuk monumen ini berupa tugu yang berjumlah lima buah bambu. Dengan bagian puncaknya dibuat meruncing berhadapan satu sama lain dan tingginya 17 meter sebagai Replika Kelima Sila Pancasila. Dan gambaran komitmen untuk senantiasa memelihara “Persatuan Kesatuan Bangsa”.
+
+        Pada bagian tengah terdapat kolam berbentuk lima tiang pancang (melambangkan Pancasila) dan pancaran air mancur sebanyak 18 buah. Kolam ini menggambarkan nikmat Allah SWT, yang sangat besar bagi daerah Bekasi. Di bagian belakang tugu ini terdapat Relief yang menggambarkan perjuangan rakyat Bekasi dalam merebut dan mempertahankan kemerdekaan dan tata kehidupan masyarakatnya yang kuat dan dipaparkannya dalam empat periode, sebagai berikut:
+
+        a. Periode pertama, Bekasi pada masa pemerintahan Hindia Belanda<br>
+        b. Periode kedua, Bekasi pada masa penduduk Jepang.<br>
+        c. Periode ketiga, masa Revolusi Kemerdekaan RI<br>
+        d. Periode keempat, era pembangunan di Bekasi`,
+        img: "https://bekasikota.go.id/uploads/pages/20211004182541-2021-10-04pages182536_thumb.jpg"
+    },
+    {
+        title: "Gedung Papak",
+        isi: `Gedung bersejarah terletak di Jalan H. Juanda Kecamatan Bekasi Timur Kota Bekasi. Gedung ini merupakan salah satu bangunan bersejarah yang turut memberikan kesaksian atas perjuangan rakyat Bekasi pada masa revolusi fisik.
+
+        Secara historis, gedung Papak ini dahulu milik seorang keturunan Tionghoa bernama Lee Guan Chin. Dia seorang pengusaha yang memiliki banyak pabrik penggilingan beras. Namun, yang paling dicatat Guan Chin ternyata memiliki loyalitas yang tinggi terhadap perjuangan rakyat Bekasi. Juga Guan Chin memiliki hubungan yang baik dengan gerakan kerakyatan pimpinan KH Noer Alie. Bahkan, gedung itu diserahkan secara sukarela sebagai salah satu markas perjuangan rakyat Bekasi.
+
+        Pada masa revolusi fisik, Gedung Papak dijadikan salah satu basis pertahanan di front pertahanan Bekasi-Jakarta (Kamaly 1983). Pertempuran-pertempuran terus berlangsung di sekitar daerah Jakarta Timur hingga Bekasi. Bahkan, pertempuran yang dahsyat terjadi di Bekasi dan sekitarnya berlangsung antara bulan September 1945 hingga 1949.
+
+        Setelah perang kemerdekaan, sejak tahun 1967 Gedung Papak menjadi rumah Dinas Bupati pada masa Bupati Soebandi. Bahkan, pada jabatan periode pertama (hingga tahun 1982) Bupati H Abdul Fatah sempat menempatinya. Pada tahun 1982 seiring pembentukan Kota Administratif Bekasi, Gedung Papak kemudian menjadi rumah dinas wali kota sejak masa wali kota H Soejono hingga masa H Kailani.
+
+        Oleh karena itu, tanpa mengubah bentuk bangunan sangat baik bila dimanfaatkan sebagai ruang publik dalam rangkaian peningkatan kecitraan atau kecintaan pada daerah ataupun budaya daerah Bekasi. Hal ini dapat difungsikan sebagai ruang perpustakaan, galeri, dan lain sebagainya.`,
+        img: "https://bekasikota.go.id/uploads/gallery/75d4d2098e7ee1938de088c78373eb7c.jpg"
+    },
+    {
+        title: "Monumen Tugu Agus Salim",
+        isi: `Monumen yang terbuat dari batu persegi empat ini terletak di Jalan KH Agus Salim, Bekasi Timur, Kota Bekasi. Pada bagian atasnya memiliki kepala yang dikelilingi oleh pecahan peluru meriam, mortir, granat tangan dan selongsong peluru ukuran 12.7 milimeter.
+
+        Sekilas keberadaan tugu dengan tinggi sekitar 205 centimeter ini hanyalah sebuah tugu batas wilayah biasa. Namun jika ditelisik dari sisi sejarah, tugu ini memiliki makna historis yang sangat besar, yang menunjukan bagaimana perjuangan masyarakat Bekasi pada masa penjajahan melawan para tentara Belanda dan sekutu.
+
+        Tidak ada sumber data yang otentik tentang nama persis monumen berupa tugu yang berada di tengah-tengah pertigaan di jalan KH Agus Salim ini. Namun masyarakat sekitar menyebutnya dengan nama tugu pahlawan. Dinamakan demikian karena pembangunan tugu ini bertujuan untuk mengenang jasa para pahlawan yang berjuang dengan hati di saat masa penjajahan kolonial Belanda, hingga pada tragedi berdarah Bekasi Lautan Api.
+
+        Kota Bekasi sebagai salah satu distrik berkumpulnya para pejuang di daerah Jawa Barat, tidak luput dari tragedi berdarah tersebut, dengan hampir seluruh bagian Kota Bekasi luluh lantak rata dengan tanah karena dibakar dan dibombardir oleh serangan udara tentara Belanda dan sekutu. Tidak terkecuali di daerah sekitar jalan KH. Agus Salim ini.
+
+        Pembakaran Kota Bekasi sendiri dilatarbelakangi oleh kemarahan seorang panglima tentara sekutu, Jendral Cristison. Dia murka atas penawanan tentara sekutu oleh rakyat Bekasi bersama pejuang RI lainnya di saat pesawat yang ditumpangi tentara sekutu yang hendak membawa mereka ke Jakarta terjatuh di daerah Rawa Gatal, Cakung.
+
+        Masyarakat dan para pejuang Republik Indonesia yang melihat adanya pesawat jatuh di daerah mereka, bergegas menghampiri dan hendak menolong. Namun setelah mengetahui seluruh penumpang pesawat merupakan tentara Belanda, masyarakat dan pejuang memutuskan untuk menahannya.
+
+        Tidak lama setelah penawanan tentara sekutu yang dilakukan oleh rakyat Bekasi dan pejuang RI, Jenderal Cristison mengeluarkan ultimatum agar tentara yang ditawan segera dibebaskan, jika tidak maka dia mengancam akan membumi hanguskan Bekasi. Namun ultimatum tersebut tidak diindahkan oleh para pejuang, bahkan para tahanan pada akhirnya dijatuhi hukuman revolusi berupa hukuman mati, dan dikubur di markas polisi Bekasi.
+
+        Melihat ultimatumnya tidak mendapat respon positif dari rakyat Bekasi, pada awal Desember 1945 secara tiba-tiba sekutu melakukan serangan pada distrik-distrik perjuangan di Bekasi, dan membawa seluruh jasad tentara sekutu yang ditawan dan dibunuh ke Jakarta. Selanjutnya pada tanggal 13 Desember 1945, tentara sekutu kembali melakukan serangan dengan menjatuhkan 200 mortir meriam di daerah Bekasi, yang kemudian membakar habis Bekasi sampai luluh lantak dengan tanah. Para pejuang pun dipukul mundur dan menyelamatkan diri ke daerah Tambun, sekitar batas timur wilayah Bekasi.
+
+        Selain itu keberadaan pertigaan yang sekarang dinamakan dengan Jalan KH. Agus Salim pada saat zaman penjajahan merupakan pintu gerbang, yang hanya akan dibuka jika tentara Belanda dan sekutu, termasuk tentara Jepang akan melewatinya. Selain itu pintu akan selalu ditutup. Hal itulah yang menjadi alasan mengapa pembangunan tugu diletakan tepat di tengah-tengah pertigaan jalan KH. Agus Salim.`,
+        img: "https://bekasikota.go.id/uploads/gallery/81d2bdfa0d08beef6de5095c16c1752a.jpg"
+    }
+];
+
+
+ function bangunanFunc() {
+    console.log("start");
+    return {
+      bangunan: bangunanData,
+      modalOpen: false,
+      modalContent: '',
+      Openmodal(data) {
+        this.modalContent = data.isi;
+        this.modalOpen = true;
+      }
+    };
+  }
